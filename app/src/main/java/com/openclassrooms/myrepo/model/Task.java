ebede310da54f3949e5bdbc4430d71c9313d5829
Vincent Dos Santos <vincedos@hotmail.com>
2025-08-31 03:43:49 +0200

    Premier commit

---
+++ app/src/main/java/com/openclassrooms/myrepo/model/Task.java
@@ -0,0 +1,103 @@
+package com.openclassrooms.myrepo.model;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * Une classe représentant une tâche avec une description.
+ */
+public class Task {
+    private String description;
+
+    /**
+     * Constructeur pour créer une nouvelle tâche avec une description.
+     *
+     * @param description La description de la tâche.
+     */
+    public Task(String description, Date dueTime) {
+
+        this.description = description;
+        this.dueTime = dueTime;
+    }
+
+
+
+    /**Partie modifié ajout date limite*/
+    private Date dueTime;
+
+    public Date getDueTime() {
+
+        return dueTime;
+
+    }
+
+    public void setDueTime(Date dueTime) {
+
+        this.dueTime = dueTime;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * Obtient la description de la tâche.
+     *
+     * @return La description de la tâche.
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Modifie la description de la tâche.
+     *
+     * @param description La nouvelle description de la tâche.
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Vérifie si deux objets Task sont égaux en comparant leurs descriptions.
+     *
+     * @param o L'objet à comparer.
+     * @return Vrai si les descriptions sont égales, sinon faux.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(description, task.description) && Objects.equals(dueTime, task.dueTime);
+    }
+
+
+/**
+     * Calcule le code de hachage en utilisant la description de la tâche.
+     *
+     * @return Le code de hachage calculé.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(description, dueTime);
+    }
+
+
+
+
+
+}
